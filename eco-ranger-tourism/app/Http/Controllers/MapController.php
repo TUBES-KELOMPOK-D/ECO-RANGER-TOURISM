073876ec400db357131e138070795e1af878cf54
Ahmad Akmal Amran <ranaeza21@gmail.com>
2026-04-11 08:27:08 +0700

    ERT-28 Layer Data Lingkungan pada Peta

--- eco-ranger-tourism/app/Http/Controllers/MapController.php
+++ eco-ranger-tourism/app/Http/Controllers/MapController.php
@@ -20,6 +20,38 @@ class MapController extends Controller
                 'lng' => 107.03, 
                 'status' => 'green',
                 'description' => 'Lokasi Penyeimbangan Karbon. Area penanaman mangrove.'
+            ],
+            [
+                'id' => 2, 
+                'name' => 'Taman Nasional Komodo, NTT', 
+                'lat' => -8.55, 
+                'lng' => 119.42, 
+                'status' => 'yellow',
+                'description' => 'Area Konservasi. Pengunjung wajib didampingi ranger.'
+            ],
+            [
+                'id' => 3, 
+                'name' => 'Gunung Bromo, Jawa Timur', 
+                'lat' => -7.94, 
+                'lng' => 112.95, 
+                'status' => 'red',
+                'description' => 'Status Waspada. Aktivitas vulkanik meningkat.'
+            ],
+            [
+                'id' => 4, 
+                'name' => 'Raja Ampat, Papua Barat', 
+                'lat' => -0.58, 
+                'lng' => 130.25, 
+                'status' => 'green',
+                'description' => 'Surga Bawah Laut. Spot diving terbaik dunia.'
+            ],
+            [
+                'id' => 5, 
+                'name' => 'Pantai Kuta, Bali', 
+                'lat' => -8.71, 
+                'lng' => 115.16, 
+                'status' => 'red',
+                'description' => 'Area Pantau: Terdapat laporan tumpukan sampah plastik.'
             ]
         ];
         
