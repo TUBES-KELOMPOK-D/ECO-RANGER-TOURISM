c57018d2b5e8900e64e47fd5fccdca81f03fa5f6
Ahmad Akmal Amran <ranaeza21@gmail.com>
2026-04-11 07:47:46 +0700

    ERT-28 PBI-05 Layer Data Lingkungan pada Peta

--- eco-ranger-tourism/app/Http/Controllers/MapController.php
+++ eco-ranger-tourism/app/Http/Controllers/MapController.php
@@ -11,7 +11,24 @@ class MapController extends Controller
 {
     public function index()
     {
+        // data dummy
         $locations = [
+            [
+                'id' => 1, 
+                'name' => 'Muara Gembong, Jawa Barat', 
+                'lat' => -6.01, 
+                'lng' => 107.03, 
+                'status' => 'green',
+                'description' => 'Lokasi Penyeimbangan Karbon. Area penanaman mangrove.'
+            ],
+            [
+                'id' => 5, 
+                'name' => 'Pantai Kuta, Bali', 
+                'lat' => -8.71, 
+                'lng' => 115.16, 
+                'status' => 'red',
+                'description' => 'Area Pantau: Terdapat laporan tumpukan sampah plastik.'
+            ]
         ];
         
         return view('map', compact('locations'));
