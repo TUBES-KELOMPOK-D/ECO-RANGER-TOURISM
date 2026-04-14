957c01b49b184127942133b7bfd47905d01cdecc
Nvrna <novarinaekaputri132@gmail.com>
2026-04-14 22:42:01 +0700

    ERT-46
    Membuat PBI-16: Sistem Perhitungan Poin Poin dari laporan, event, aktivitas, tetapi masih in progress

--- eco-ranger-tourism/app/Models/User.php
+++ eco-ranger-tourism/app/Models/User.php
@@ -52,4 +52,80 @@ class User extends Authenticatable
             'eco_points' => 'integer',
         ];
     }
+
+    /**
+     * Get user's achievements
+     */
+    public function achievements()
+    {
+        return $this->hasMany(UserAchievement::class);
+    }
+
+    /**
+     * Get user's actions (community participation)
+     */
+    public function actions()
+    {
+        return $this->hasMany(Action::class);
+    }
+
+    /**
+     * Get user's reports
+     */
+    public function reports()
+    {
+        return $this->hasMany(Report::class);
+    }
+
+    /**
+     * Calculate eco level based on points
+     */
+    public function calculateEcoLevel()
+    {
+        $points = $this->eco_points ?? 0;
+
+        if ($points >= 2400) {
+            return 'ECO-RANGER';
+        } elseif ($points >= 2000) {
+            return 'ECO-WARRIOR';
+        } elseif ($points >= 1000) {
+            return 'ECO-LEADER';
+        } else {
+            return 'ECO-MEMBER';
+        }
+    }
+
+    /**
+     * Get user rank in global leaderboard
+     */
+    public function getRankAttribute()
+    {
+        return self::where('eco_points', '>', $this->eco_points)
+            ->count() + 1;
+    }
+
+    /**
+     * Add eco points
+     */
+    public function addEcoPoints($points, $description = null)
+    {
+        $this->eco_points = ($this->eco_points ?? 0) + $points;
+        $this->eco_level = $this->calculateEcoLevel();
+        $this->save();
+
+        return $this;
+    }
+
+    /**
+     * Get user's initials
+     */
+    public function getInitialsAttribute()
+    {
+        $names = explode(' ', $this->name);
+        $initials = '';
+        foreach ($names as $name) {
+            $initials .= strtoupper(substr($name, 0, 1));
+        }
+        return substr($initials, 0, 2);
+    }
 }
--- eco-ranger-tourism/routes/web.php
+++ eco-ranger-tourism/routes/web.php
@@ -4,12 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MapController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RankingController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Middleware\AdminMiddleware;
 
 // --Home--
 Route::get('/', [MapController::class, 'index']);
+
+// --Rankings (Public)--
+Route::get('/peringkat', [RankingController::class, 'index'])->name('peringkat.index');
+Route::get('/peringkat/dashboard', [RankingController::class, 'dashboard'])->name('peringkat.dashboard');
+Route::get('/api/peringkat/leaderboard', [RankingController::class, 'getLeaderboardJson'])->name('peringkat.leaderboard.json');
+Route::get('/api/peringkat/point-rules', [RankingController::class, 'getPointRulesJson'])->name('peringkat.rules.json');
 
 // --Public--
 Route::middleware('guest')->group(function () { 
@@ -28,6 +35,10 @@ Route::middleware('auth')->group(function () {
     Route::post('/profile/settings', [ProfileController::class, 'update'])->name('profile.settings.update');
 
     Route::get('/reports', [ReportController::class, 'index'])->name('reports.index');
+
+    // --Achievements--
+    Route::get('/pencapaian/{user}', [RankingController::class, 'achievements'])->name('pencapaian.index');
+    Route::get('/api/user/pencapaian/{user}', [RankingController::class, 'getUserAchievementsJson'])->name('user.pencapaian.json');
 
     Route::prefix('admin')->middleware(AdminMiddleware::class)->group(function () {
         Route::post('/markers', [MapController::class, 'store'])->name('markers.store');
