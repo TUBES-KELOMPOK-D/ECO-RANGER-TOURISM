215da7d2af66402f98e6959ff46a82c9ae6fc61e
Nvrna <novarinaekaputri132@gmail.com>
2026-05-03 18:33:24 +0700

    ERT-48
    PBI-18 : Badge atau Achievement Reward berupa badge untuk user aktif

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -41,6 +41,7 @@
                             </div>
                             @if(auth()->user()->role === 'user')
                                 <a href="/profile" class="block px-4 py-3 text-sm font-semibold text-slate-900 hover:bg-slate-50">Profil Saya</a>
+                                <a href="/reports" class="block px-4 py-3 text-sm font-semibold text-slate-900 hover:bg-slate-50">Pantau Laporan</a>
                             @else
                                 <a href="/admin/dashboard" class="block px-4 py-3 text-sm font-semibold text-emerald-700 hover:bg-emerald-50">Panel Admin</a>
                                 <a href="{{ route('markers.index') }}" class="block px-4 py-3 text-sm font-semibold text-emerald-700 hover:bg-emerald-50">Kelola Marker</a>
@@ -126,6 +127,11 @@
                     <a href="/reports" class="flex items-center gap-3 w-full p-4 rounded-2xl font-bold text-slate-700 hover:bg-slate-50 transition-all">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/></svg>
                         Kelola Laporan
+                    </a>
+                    @elseif(auth()->user()->role === 'user')
+                    <a href="/reports" class="flex items-center gap-3 w-full p-4 rounded-2xl font-bold text-slate-700 hover:bg-slate-50 transition-all">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><polyline points="10 9 9 9 8 9"/></svg>
+                        Pantau Laporan
                     </a>
                     @endif
 
